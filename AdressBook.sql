454130420d8a075d149e369ac49883082564418b
vivek <ikumarvivek786@gmail.com>
2022-11-04 22:27:29 +0530

    UC2_CreateTable

--- AdressBook.sql
+++ AdressBook.sql
@@ -1,3 +1,15 @@
 --Uc1 create AddressBook Database
 create database Address_book_service
 
+--Uc2 Cteate Table
+create table employee_payroll
+( 
+first_name varchar(25),
+last_name varchar(30),
+address varchar(200),
+city varchar(25),
+state varchar(30),
+zip varchar(6),
+Phone_number varchar(15),
+email varchar(50)
+)
