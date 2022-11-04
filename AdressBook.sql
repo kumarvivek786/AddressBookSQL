6d2387fde7916346055e7c6d0c49f2c88ce494d8
vivek <ikumarvivek786@gmail.com>
2022-11-04 22:39:10 +0530

    UC3_InsertRecord

--- AdressBook.sql
+++ AdressBook.sql
@@ -13,3 +13,13 @@ zip varchar(6),
 Phone_number varchar(15),
 email varchar(50)
 )
+
+--uc3 Insert records
+insert into employee_Payroll values
+
+('kumar','vivek','Gurhatta','Patna','Bihar',800008,9934776820,'ikumarvivek786@gmail.com'),
+('Shashi','kumar','VijayNagar','Muzaffarpur','Bihar',800001,1234567896,'Shashi1234@gmail.com'),
+('Rakesh','kumar','Patliputra','patna','Bihar',800002,9912000001,'rakesh@gmail.com'),
+('Harsh','kumar','Kankerbagh','Ara','Bihar',800003,9141253625,'Harsh@gmail.com');
+
+select * from employee_payroll
